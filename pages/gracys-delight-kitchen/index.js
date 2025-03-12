85777c4a747735491adc994b171ac5d46d77c821
tceeokoye <tceeservices@gmail.com>
2025-03-12 14:45:35 +0100

    changes

--- pages/gracys-delight-kitchen/index.js
+++ pages/gracys-delight-kitchen/index.js
@@ -8,29 +8,22 @@ import ShopInfo from "@/components/shop/catering/ShopInfo";
 const whatsappNumber = "+2349163903617";
 
 const products = [ 
-  { name: "Special bons", imageUrl: "special-bons.jpg" },
-  { name: "Egg Roll", imageUrl: "egg-roll.jpg" },
+  { name: "Peppered-Beaf", imageUrl: "Peppered-beaf.svg" },
+  { name: "Jollof rice", imageUrl: "Jollof-rice.svg"},
   { name: "Food Tray", imageUrl: "food-tray.jpg" },
-  { name: "Chin Chin", imageUrl: "chin-chin.jpg" },
-  { name: "Fruit cake", imageUrl: "fruit-cake.jpg" },
-  { name: "Donut", imageUrl: "donut.jpg" }, 
-  { name: "Puf Puf", imageUrl: "puf-puf.jpg" },
-  { name: "Naked Cake", imageUrl: "naked-cake.jpg" },
-  { name: "Special Meatpie", imageUrl: "special-meatpie.jpg" },
-  { name: "Banana Bread", imageUrl: "banana-bread.jpg" },
-  { name: "Foil Cake", imageUrl: "foil-cake.jpg" },
-  { name: "All flavors Cake", imageUrl: "all-flavors.jpg" }, 
-  { name: "Whipped Cream Iced Cake", imageUrl: "whipped-cream-iced-cake.jpg" },
-  { name: "Butter Iced Cake", imageUrl: "butter-iced-cake.jpg" },
-  { name: "Cup Cake", imageUrl: "cup-cake.jpg" },
-  { name: "Cake parfait", imageUrl: "cake-parfait.jpg" },
-  { name: "Fondant Cake", imageUrl: "fondant-cake.jpg" },
+  { name: "Fried rice", imageUrl: "fried-rice.svg" },
+  { name: "Fried rice", imageUrl: "fried-rice2.svg" },
+  { name: "Peppered snail", imageUrl: "Peppered-snail.svg" }, 
+  { name: "Jollof spaggetti", imageUrl: "spaggetti.svg" },
+  { name: "Peppered Beaf", imageUrl: "Peppered-beaf2.svg"},
+  { name: "Vegetable soup", imageUrl: "vagetable-soup.svg" },
+  { name: "Jollof rice", imageUrl: "jollof-rice2.svg" },
+  { name: "Small Chops", imageUrl: "small-chops.svg" },
+  { name: "take away", imageUrl: "takeaway.svg" }, 
+  { name: "Small Chops & Peppered Chicken", imageUrl: "small-chops-pappered-chicken.svg" },
+  { name: "cooler of jollof rice", imageUrl: "cooler-jollof-rice.svg" },
+  { name: "Peppered chicken", imageUrl: "Peppered-chicken.svg" },
   { name: "Small Chops", imageUrl: "small-chops.jpg" },
-  { name: "White Wedding Cakes", imageUrl: "white-wedding-cake.jpg" },
-  {
-    name: "Traditional Wedding Cakes",
-    imageUrl: "traditional-wedding-cake.jpg",
-  },
 ];
 
 const Gracekitchen = () => {
@@ -38,39 +31,39 @@ const Gracekitchen = () => {
     <>
       <Head>
         <title>
-        Gracy's Delight Kitchen - Custom Cakes and Pastries on Order
+        Gracy's Delight Kitchen - Satisfying you with your budget African and contenental Dishes, Small Chops Smoothie Etc
         </title>
         <meta
           name="description"
-          content="We specialize in baking cakes and pastries tailored to your preferences. Whether you're celebrating a birthday, wedding, or any special occasion, our delicious treats are crafted with care and creativity. Order now to experience the sweetness of Splendor!"
+          content="We specialize in African and contenental Dishes, Small Chops Smoothie Etc tailored to your preferences. Whether you're celebrating a birthday, wedding, or any special occasion, our delicious treats are crafted with care and creativity. Order now to experience the sweetness of Grace's!"
         />
         <meta
           name="keywords"
-          content="Gracy's Delight Kitchen, custom cakes, custom pastries, cake orders, pastry orders, celebration cakes, wedding cakes, birthday cakes, special occasion cakes, artisan cakes, gourmet pastries, bakery orders"
+          content="Gracy's Delight Kitchen, African and contenental Dishes, Small Chops Smoothie Etc"
         />
         <meta name="author" content="Gracy's Delight Kitchen" />
         <meta
           property="og:title"
-          content="Gracy's Delight Kitchen - Custom Cakes and Pastries on Order"
+          content="Gracy's Delight Kitchen - African and contenental Dishes, Small Chops Smoothie Etc"
         />
         <meta
           property="og:description"
-          content="We specialize in baking cakes and pastries tailored to your preferences. Whether you're celebrating a birthday, wedding, or any special occasion, our delicious treats are crafted with care and creativity. Order now to experience the sweetness of Splendor!"
+          content="We specialize in African and contenental Dishes, Small Chops Smoothie Etc tailored to your preferences. Whether you're celebrating a birthday, wedding, or any special occasion, our delicious treats are crafted with care and creativity. Order now to experience the sweetness of Grace's"
         />
         <meta
           property="og:image"
-          content="https://tceetechnologies-w3fh.vercel.app/images/shop/catering/graces-kitchen-and-pasteries/graces-kitchen-and-pasteries-logo.svg"
+          content="https://tceetechnologies-w3fh.vercel.app/images/shop/catering/graces-kitchen-and-pasteries/graces-kitchen-and-pasteries-logoprev.jpg"
         />
       </Head>
 
       <ShopInfo
         header="Gracy's Delight Kitchen"
-        title="We make custom cakes and pastries on order."
+        title="Satisfying you with your budget African and contenental Dishes, Small Chops Smoothie Etc"
         logo={GracekitchenLogo}
         subTitle="Home for yummy and tasty treats"
         products={products}
-        shopImageFolder="splendor-cakes-and-pasteries"
-        whatsappNumber="+2349163903617"
+        shopImageFolder="graces-kitchen-and-pasteries"
+        whatsappNumber= {whatsappNumber}
       />
     </>
   );
